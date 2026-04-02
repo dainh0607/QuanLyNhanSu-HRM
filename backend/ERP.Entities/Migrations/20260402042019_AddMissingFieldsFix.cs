f8e19eac281e2e63e4277e06573524fd678b9fd3
Avaricia3508 <kfrog1233@gmail.com>
2026-04-02 11:22:30 +0700

    quick fix model + migration

---
+++ backend/ERP.Entities/Migrations/20260402042019_AddMissingFieldsFix.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ERP.Entities.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMissingFieldsFix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "gender",
+                table: "Dependents",
+                type: "nvarchar(10)",
+                maxLength: 10,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "gender",
+                table: "Dependents");
+        }
+    }
+}
